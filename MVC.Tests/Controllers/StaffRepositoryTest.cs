0f0313b9d25451d304d838170728f7f64e6de00b
rfritch <russfritch@gmail.com>
2015-04-12 17:30:33 -0700

    added a unit test needs moe

---
+++ MVC.Tests/Controllers/StaffRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC.Interfaces;
+using MVC.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVC.Tests.Controllers
+{
+    [TestClass]
+    public class AppointmentRepositoryTest
+    {
+        private IStaff _staff;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var repo = new StaffRepository();
+
+            //Grab the first staff member form the list
+            _staff = repo.GetStaff().First();
+        }
+
+        [TestMethod]
+        public void GetAppointmentsHasData()
+        {
+            var repo = new AppointmentRepository();
+
+            DateTime appointmentDate = DateTime.ParseExact("2014-01-03", "yyyy-MM-dd", null);
+
+            List<IAppointment> appointments = repo.GetStaffAppointments(_staff, appointmentDate);
+
+            Assert.IsNotNull(appointments);
+            Assert.IsTrue(appointments.Any());
+            Assert.IsNotNull(appointments.First());
+        }
+    }
+}
+
